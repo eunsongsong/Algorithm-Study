4481e831ca69c23eb2b9cc6242734f6b371cbdd9
song <eunsonglee0204@gmail.com>
2020-04-22 01:22:03 +0900

    java,py,124나라숫자

--- JAVA/programmers Level 1/src/크레인인형뽑기게임/Test.java
+++ JAVA/programmers Level 1/src/크레인인형뽑기게임/Test.java
@@ -8,7 +8,7 @@ public class Test {
 		int[][] board = {{0,0,0,0,0},{0,0,1,0,3},{0,2,5,0,1},{4,2,4,4,2},{3,5,1,3,1}}; 
 		int[] moves = {1,5,3,5,1,2,1,4};
 		
-		System.out.println(sol.solution(board, moves));
+		System.out.println(sol.solution(board, moves) + " >> 4");
 	}
 
 }
---
+++ JAVA/programmers Level 2/src/_124나라의숫자/Test.java
@@ -0,0 +1,17 @@
+package _124나라의숫자;
+
+public class Test {
+
+	public static void main(String[] args) {
+		Solution s1 = new Solution();
+		Solution s2 = new Solution();
+		Solution s3 = new Solution();
+		Solution s4 = new Solution();
+		
+		System.out.println(s1.solution(1) + " >> 1");
+		System.out.println(s2.solution(2) + " >> 2");
+		System.out.println(s3.solution(3) + " >> 4");
+		System.out.println(s4.solution(4) + " >> 11");
+	}
+
+}
