a36257d844f443e5d6a56bcae30fa758324c9265
song <eunsonglee0204@gmail.com>
2020-10-20 10:27:29 +0900

    java

--- JAVA/programmers Level 1/src/두개뽑아서더하기/Test.java
+++ JAVA/programmers Level 1/src/두개뽑아서더하기/Test.java
@@ -1,14 +1,16 @@
 package 두개뽑아서더하기;
 
+import java.util.Arrays;
+
 public class Test {
 	public static void main(String[] args) {
 		Solution s = new Solution();
-		
-		int n1[] = {2,1,3,4,1};
-		int n2[] = {5,0,2,7};
-		
-		System.out.println(s.solution(n1).equals(new int[]{2,3,4,5,6,7}));
-		
+
+		int n1[] = { 2, 1, 3, 4, 1 };
+		int n2[] = { 5, 0, 2, 7 };
+
+		System.out.println(Arrays.equals(s.solution(n1), new int[] { 2, 3, 4, 5, 6, 7 }));
+		System.out.println(Arrays.equals(s.solution(n2), new int[] { 2, 5, 7, 9, 12 }));
 	}
 
 }
